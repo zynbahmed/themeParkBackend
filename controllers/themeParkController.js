04d990420b387c3b4fc1cbd2af0f3d1819680ab2
zynbahmed <201901241@student.polytechnic.bh>
2024-02-26 21:22:38 +0300

    controllers created

---
+++ controllers/themeParkController.js
@@ -0,0 +1,34 @@
+const { ThemePark } = require('../models')
+
+const GetParks = async (req, res) => {
+  try {
+    const parks = await ThemePark.find({})
+    res.send(parks)
+  } catch (error) {
+    throw error
+  }
+}
+
+const CreatePark = async (req, res) => {
+  try {
+    const park = await ThemePark.create({ ...req.body })
+    res.send(park)
+  } catch (error) {
+    throw error
+  }
+}
+
+const DeletePark = async (req, res) => {
+  try {
+    await ThemePark.deleteOne({ _id: req.params.themePark_id })
+    res.send({ msg: 'Park Deleted', payload: req.params.themePark_id, status: 'Ok' })
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  GetParks,
+  CreatePark,
+  DeletePark
+}
---
+++ controllers/waterParkController.js
@@ -0,0 +1,34 @@
+const { WaterPark } = require('../models')
+
+const GetParks = async (req, res) => {
+  try {
+    const parks = await WaterPark.find({})
+    res.send(parks)
+  } catch (error) {
+    throw error
+  }
+}
+
+const CreatePark = async (req, res) => {
+  try {
+    const park = await WaterPark.create({ ...req.body })
+    res.send(park)
+  } catch (error) {
+    throw error
+  }
+}
+
+const DeletePark = async (req, res) => {
+  try {
+    await WaterPark.deleteOne({ _id: req.params.waterPark_id })
+    res.send({ msg: 'Park Deleted', payload: req.params.waterPark_id, status: 'Ok' })
+  } catch (error) {
+    throw error
+  }
+}
+
+module.exports = {
+  GetParks,
+  CreatePark,
+  DeletePark
+}
