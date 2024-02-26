88f36c54bb26dae417ca4e161e518b14b0bf028d
zynbahmed <201901241@student.polytechnic.bh>
2024-02-26 21:13:21 +0300

    server + db + middleware created

--- server.js
+++ server.js
@@ -1,41 +1,28 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const ThemeParkRouter = require('./routes/themeParkRouter')
+const WaterParkRouter = require('./routes/waterParkRouter')
 
-var app = express();
+const PORT = process.env.PORT || 3001
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+const db = require('./db')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+const app = express()
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+app.use('/theme', ThemeParkRouter)
+app.use('/water', WaterParkRouter)
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use('/', (req, res) => {
+  res.send(`Connected!`)
+})
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Running Express server on Port ${PORT} . . .`)
+})
