8c1e1df2fd154524dfc49fb48baf99ffd768ae3d
zynbahmed <201901241@student.polytechnic.bh>
2024-02-27 12:45:28 +0300

    fixxxxxx

--- controllers/themeParkController.js
+++ controllers/themeParkController.js
@@ -9,6 +9,16 @@ const GetParks = async (req, res) => {
   }
 }
 
+const GetParkDetails = async (req, res) => {
+  const parkId = req.params.themePark_id
+  try {
+    const park = await ThemePark.findById(parkId)
+    res.send(park)
+  } catch (error) {
+    throw(error)
+  }
+}
+
 const CreatePark = async (req, res) => {
   try {
     const park = await ThemePark.create({ ...req.body })
@@ -29,6 +39,7 @@ const DeletePark = async (req, res) => {
 
 module.exports = {
   GetParks,
+  GetParkDetails,
   CreatePark,
   DeletePark
 }
--- routes/themeParkRouter.js
+++ routes/themeParkRouter.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const controller = require('../controllers/themeParkController')
 
 router.get('/', controller.GetParks)
+router.get('/:themePark_id', controller.GetParkDetails)
 router.post( '/', controller.CreatePark)
 router.delete('/:themePark_id', controller.DeletePark)
 
